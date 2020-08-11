50bfb9fd36b9dc9aaa82804118b986eabc122c2c
ranarupesh <rupeshrana288@gamil.com>
2020-08-11 18:57:48 +0530

    Updates

---
+++ precache-manifest.547bf3e6d608af3cf0c35bae623b9b3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79912000de61070433d901ee3cc04c2",
+    "url": "/instagram_clone/index.html"
+  },
+  {
+    "revision": "55d69804011be6a0f5b3",
+    "url": "/instagram_clone/static/css/main.2a2882c6.chunk.css"
+  },
+  {
+    "revision": "2644728fac26d0fc4229",
+    "url": "/instagram_clone/static/js/2.b903b301.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/instagram_clone/static/js/2.b903b301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d69804011be6a0f5b3",
+    "url": "/instagram_clone/static/js/main.3de792f4.chunk.js"
+  },
+  {
+    "revision": "cfe1e9f41fe1f0833a08",
+    "url": "/instagram_clone/static/js/runtime-main.f25643d1.js"
+  }
+]);
